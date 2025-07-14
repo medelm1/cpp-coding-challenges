554ff46659f351e77396c2678242cc6bff4bbff5
Med Elm <medelm.dev@gmail.com>
2025-07-15 00:54:21 +0100

    feat: implement factorial solution with recursion and iterative

--- 001-factorial/src/main.cpp
+++ 001-factorial/src/main.cpp
@@ -1,8 +1,44 @@
 #include <iostream>
 
+int factorialRecursive(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+
+    return n * factorialRecursive(n-1);
+}
+
+int factorialIterative(int n)
+{
+    int i, prod;
+
+    for (i = n, prod = 1; i >= 1; i--)
+    {
+        prod *= i;
+    }
+
+    return prod;
+}
+
 int main()
 {
-    std::cout << "Challenge 001 – Factorial" << std::endl;
+    int npt;
+
+    do
+    {
+        std::cout << "\nInput: ";
+        std::cin >> npt;
+
+        if (npt < 0)
+        {
+            std::cout << "Error: factorial is undefined for negative numbers." << std::endl;
+        }
+
+    } while (npt < 0);
+
+    std::cout << factorialIterative(npt) << std::endl;
 
     return 0;
 }
