7d352e4fed0761252499bfd6ef013e41b343a384
Med Elm <medelm.dev@gmail.com>
2025-07-17 23:39:40 +0100

    feat: solve challenge 003 - Digit Frequency Counter

--- 003-digit-frequency-counter/src/main.cpp
+++ 003-digit-frequency-counter/src/main.cpp
@@ -2,5 +2,30 @@
 
 int main(void)
 {
-    std::cout << "Challenge 003 - Digit Frequency Counter" << std::endl;
+    int SIZE = 10;
+    int frequencies[SIZE];
+    int i;
+    std::string input;
+
+    // Init frequencies
+    for (i = 0; i < SIZE; i++)
+    {
+        frequencies[i] = 0;
+    }
+
+    // Get number
+    std::cin >> input;
+
+    // Calculate each digit frequency
+    for (i = 0; i < input.length(); i++)
+    {
+        frequencies[input[i] - '0'] += 1;
+    }
+
+    // Display result
+    std::cout << "\n";
+    for (i = 0; i < SIZE; i++)
+    {
+        std::cout << "Digit " << i << ": " << frequencies[i] << std::endl;
+    }
 }
