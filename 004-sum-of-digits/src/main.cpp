b28f58fa7c604ccedd252c8107959df1da4336b6
Med Elm <medelm.dev@gmail.com>
2025-07-18 00:11:06 +0100

    feat: solve challenge 004 - Sum of Digits

--- 004-sum-of-digits/src/main.cpp
+++ 004-sum-of-digits/src/main.cpp
@@ -2,5 +2,22 @@
 
 int main(void)
 {
-    std::cout << "Challenge 004 - Sum of digits" << std::endl;
+    int number, sum;
+
+    // Get user number
+    do
+    {
+        std::cin >> number;
+    } while (number <= 0);
+
+    // Calculate the sum
+    sum = 0;
+    while (number > 0)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+
+    // Display results
+    std::cout << "\nSum of digits: " << sum << std::endl;
 }
