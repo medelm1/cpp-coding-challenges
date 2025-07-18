0ea679c388a7c9a32a2ebcd5bbc98ab2310e92ee
Med Elm <medelm.dev@gmail.com>
2025-07-18 22:16:42 +0100

    ✅ solve: Challenge 008 - Most Frequent Digit

--- 008-most-frequent-digit/src/main.cpp
+++ 008-most-frequent-digit/src/main.cpp
@@ -1,6 +1,47 @@
 #include <iostream>
+#include <utility>
+
+std::pair<int, int> mostFrequentDigit(long);
 
 int main(void)
 {
-    std::cout << "Challenge 008 - Most Frequent Digit" << std::endl;
+    long n;
+
+    do
+    {
+        std::cout << "Enter a positive integer: ";
+        std::cin >> n;
+
+        if (n < 0) std::cout << "Error: invalid integer\n\n";
+    }
+    while (n < 0);
+    
+    std::pair result = mostFrequentDigit(n);
+
+    std::cout << "Most frequent digit: " << result.first << " (frequency: " << result.second << ")" << std::endl;
+}
+
+std::pair<int, int> mostFrequentDigit(long n)
+{
+    const int SIZE = 10;
+    long temp;
+    int frequencies[SIZE], i, mostFrequentDigit;
+
+    // initialization
+    for (i = 0; i < SIZE; frequencies[i++] = 0);
+
+    // count digits
+    for (temp = n; temp != 0; frequencies[temp % 10] += 1, temp /= 10);
+
+    // get most frequent digit
+    for (i = 1, mostFrequentDigit = 0; i < SIZE; i++)
+    {
+        if (frequencies[mostFrequentDigit] < frequencies[i])
+        {
+            mostFrequentDigit = i;
+        }
+    }
+
+    // return the most frequent digit & its frequency
+    return std::make_pair(mostFrequentDigit, frequencies[mostFrequentDigit]);
 }
