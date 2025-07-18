f30a8215ba2612768a32c037b1316af8027cc012
Med Elm <medelm.dev@gmail.com>
2025-07-18 21:14:35 +0100

    ✅ solve: Challenge 007 - Palindrome Number Checker

--- 007-palindrome-number-checker/src/main.cpp
+++ 007-palindrome-number-checker/src/main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
 
+bool isPalindrome(int n);
+
 int main(void)
 {
-    std::cout << "Challenge 007 - Palindrome Number Checker" << std::endl;
+    int n;
+
+    std::cin >> n;
+
+    if (isPalindrome(n))
+    {
+        std::cout << "Palindrome" << std::endl;
+    }
+    else
+    {
+        std::cout << "Not a palindrome" << std::endl;
+    }
+}
+
+bool isPalindrome(int n)
+{
+    int tmp, reversed;
+
+    tmp = n;
+    reversed = 0;
+
+    do
+    {
+        reversed = reversed*10 + (tmp % 10);
+        tmp /= 10;
+    } 
+    while (tmp != 0);
+
+    return reversed == n;
 }
