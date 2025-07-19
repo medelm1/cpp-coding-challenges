23808e7e98b5bed354e74aeb1968037248e34a70
Med Elm <medelm.dev@gmail.com>
2025-07-19 17:00:09 +0100

    ✅ solve: Challenge 009 - Armstrong Number Checker

--- 009-armstrong-number-checker/src/main.cpp
+++ 009-armstrong-number-checker/src/main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include <cmath>
+
+bool isArmstrong(int);
 
 int main(void)
 {
-    std::cout << "Challenge 009 - Armstrong Number Checker" << std::endl;
+    int n;
+
+    do {
+        std::cout << "Enter a positive integer: ";
+        std::cin >> n;
+
+    } while (n < 0);
+
+    std::cout << (isArmstrong(n) ? "Armstrong" : "Not an Armstrong number") << std::endl;
+}
+
+bool isArmstrong(int n)
+{
+    int temp, sum, numberOfDigits;
+
+    for (
+        temp = n, numberOfDigits = 0; 
+        temp != 0; 
+        numberOfDigits++, temp /= 10
+    );
+    for (
+        temp = n, sum = 0; 
+        temp != 0; 
+        sum += pow((temp % 10), numberOfDigits), temp /= 10
+    );
+
+    return sum == n;
 }
