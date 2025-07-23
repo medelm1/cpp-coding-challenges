ffb7e45db18c34ae851b79336ede3c33a803947c
Med Elm <medelm.dev@gmail.com>
2025-07-23 18:55:49 +0100

    ✅ solve: Challenge 011 - FizzBuzz Range

--- 011-fizzbuzz-range/src/main.cpp
+++ 011-fizzbuzz-range/src/main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
 
+void fizzBuzz(int);
+
 int main(void)
 {
-    std::cout << "Challenge 011 - Fizz Buzz Range" << std::endl;
+    int N;
+    
+    std::cout << "Enter an integer: "; std::cin >> N;
+
+    fizzBuzz(N);
+}
+
+void fizzBuzz(int n)
+{
+    int i;
+    bool divisibleBy3, divisibleBy5;
+
+    for (i = 1; i <= n; i++)
+    {
+        divisibleBy3 = i % 3 == 0;
+        divisibleBy5 = i % 5 == 0;
+
+        if (!divisibleBy3 && !divisibleBy5)
+        {
+            std::cout << i;
+        }
+        else
+        {
+            std::cout << (divisibleBy3 ? "Fizz" : "");
+            std::cout << (divisibleBy5 ? "Buzz" : "");
+        }
+        std::cout << "\n";
+    }
 }
