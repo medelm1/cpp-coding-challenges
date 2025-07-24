d49b52f9eacece169b69754f62551546056b2d98
Med Elm <medelm.dev@gmail.com>
2025-07-24 23:37:09 +0100

    ✅ solve: Challenge 016 - Reverse Array

--- 016-reverse-array/src/main.cpp
+++ 016-reverse-array/src/main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
 
+void reverseArray(int[], int);
+
 int main(void)
 {
-    std::cout << "Challenge 016 - Reverse Array" << std::endl;
+    int iter, n;
+    do {
+        std::cout << "Size: "; std::cin >> n;
+    }
+    while (n < 1 || n > 100);
+
+    int arr[n];
+    for (iter = 0; iter < n; iter++)
+    {
+        std::cout << iter << "-th element: "; std::cin >> arr[iter];
+    }
+
+    reverseArray(arr, n);
+
+    // print revsersed array
+    for (iter = 0; iter < n; iter++)
+    {
+        std::cout << arr[iter] << '\t';
+    }
+}
+
+void reverseArray(int arr[], int n)
+{
+    int start, end, temp;
+
+    for (start = 0, end = n-1; start < end; start++, end--)
+    {
+        temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+    }
 }
