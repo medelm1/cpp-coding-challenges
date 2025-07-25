4de05b9b98753518f7012efd3a60300f69570a21
Med Elm <medelm.dev@gmail.com>
2025-07-25 15:58:05 +0100

    ✅ solve: Challenge 018 - Balanced Brackets

--- 018-balanced-brackets/src/main.cpp
+++ 018-balanced-brackets/src/main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
 
+bool isBalanced(const std::string&);
+
 int main(void)
 {
-    std::cout << "Challenge 018 - Balanced Brackets" << std::endl;
+    std::string brackets;
+
+    std::cout << "Enter brackets: ";
+    std::getline(std::cin, brackets);
+
+    std::cout << (isBalanced(brackets) ? "YES" : "NO");
+}
+
+bool isBalanced(const std::string& str)
+{
+    int BRACKETS_SIZE = 6;
+    char brackets[BRACKETS_SIZE] = {'(', ')', '[', ']', '{', '}'};
+    int start, end, iter;
+
+    for (start = 0, end = str.length()-1; start < end; start++, end--)
+    {
+        for (iter = 0; iter < BRACKETS_SIZE; iter++)
+        {
+            if (str[start] == brackets[iter])
+            {
+                if ((iter + 1) >= BRACKETS_SIZE || str[end] != brackets[iter+1])
+                {
+                    return false;
+                }
+            }
+        }
+    }
+
+    return true;
 }
