d2f45fdeb4788c6c4d0bcf0447f5ee97c6b89fc8
Med Elm <medelm.dev@gmail.com>
2025-07-25 16:20:33 +0100

    ✅ solve: Challenge 019 - Decimal to Binary

--- 019-decimal-to-binary/src/main.cpp
+++ 019-decimal-to-binary/src/main.cpp
@@ -1,6 +1,47 @@
 #include <iostream>
+#include <cmath>
+
+std::string _decimalToBinary(int);
 
 int main(void)
 {
-    std::cout << "Challenge 019 - Decimal to Binary" << std::endl;
+    unsigned int n;
+
+    do
+    {
+        std::cout << "Enter a positive integer: ";
+        std::cin >> n;
+    }
+    while (n < 0 || n > pow(10, 9));
+
+    std::cout << "\n";
+    std::cout << _decimalToBinary(n);
+}
+
+std::string _decimalToBinary(int n)
+{
+    int quotient, remainder, start, end;
+    char temp;
+    std::string binary;
+
+    quotient = n;
+
+    do
+    {
+        remainder = (quotient % 2);
+        quotient /= 2; 
+    
+        binary += std::to_string(remainder);
+    }
+    while (quotient != 0);
+
+    // reverse the binary string
+    for (start = 0, end = (binary.length() - 1); start < end; start++, end--)
+    {
+        temp = binary[start];
+        binary[start] = binary[end];
+        binary[end] = temp;
+    }
+
+    return binary;
 }
