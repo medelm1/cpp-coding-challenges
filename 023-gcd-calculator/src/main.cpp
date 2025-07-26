0e1415ad3f3e0dc9b4fd47ed079b4a0f748f75e5
Med Elm <medelm.dev@gmail.com>
2025-07-26 22:28:19 +0100

    ✅ solve: Challenge 023 - GCD Calculator

--- 023-gcd-calculator/src/main.cpp
+++ 023-gcd-calculator/src/main.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
 
+int GCD_iterative(int, int);
+int GCD_recursive(int, int);
+
 int main(void)
 {
-    std::cout << "Challenge 023 - GCD Calculator" << std::endl;
+    int num1, num2;
+
+    do { std::cout << "Enter first number: "; std::cin >> num1; } while (num1 <= 0);
+    do { std::cout << "Enter second number: "; std::cin >> num2; } while (num2 <= 0);
+
+    std::cout << "\n";
+    std::cout << "GCD(" << num1 << ", " << num2 << ")= " << GCD_recursive(num1, num2);
+}
+
+int GCD_recursive(int a, int b)
+{
+    if (b == 0)
+    {
+        return a;
+    }
+
+    return GCD_recursive(b, a % b);
+}
+
+int GCD_iterative(int a, int b)
+{
+    int remainder;
+
+    do
+    {
+        remainder = a % b;
+        a = b;
+        b = remainder;
+    }
+    while (remainder != 0);
+
+    return a;
 }
