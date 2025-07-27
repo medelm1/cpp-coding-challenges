a9bfdece0e884ee88b1d17d26f9275a81713e2c7
Med Elm <medelm.dev@gmail.com>
2025-07-27 16:47:51 +0100

    ✅ solve: Challenge 025 - Run Length Encoding

--- 025-run-length-encoding/src/main.cpp
+++ 025-run-length-encoding/src/main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
 
+std::string runLengthEncoding(const std::string&);
+
 int main(void)
 {
-    std::cout << "Challenge 025 - Run Length Encoding" << std::endl;
+    std::string str;
+
+    std::cout << "Enter a sentence: ";
+    std::getline(std::cin, str);
+
+    std::cout << "\n";
+    std::cout << "RLE: ";
+    std::cout << runLengthEncoding(str);
+}
+
+std::string runLengthEncoding(const std::string& str)
+{
+    int i, j, counter;
+    std::string compressedStr;
+
+    for (i = 0; i < str.length();)
+    {
+        for (j = i, counter = 1; j < str.length() && std::tolower(str[i]) == std::tolower(str[j+1]); j++, counter++);
+
+        compressedStr += str[i] + std::to_string(counter);
+
+        i = j+1;
+    }
+
+    return compressedStr;
 }
